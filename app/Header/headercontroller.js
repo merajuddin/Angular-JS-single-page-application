3eb376ca8f41df02e0e2330b45c72b2a2ee9712a
mohammed merajuddin <meraj6054@gmail.com>
2017-03-24 01:27:21 -0500

    Home page layout enhancments
    some minor improvements

--- app/Header/headercontroller.js
+++ app/Header/headercontroller.js
@@ -35,7 +35,7 @@ $scope.BuyItem = function(){
 
 $scope.selectedItem = JSON.parse(localStorage.getItem('currentGuitar'));
 
-/*$scope.selectedItem.noofitems = parseInt($scope.selectedItem.noofitems);*/
+/*$scope.selectedItem.noofitems = parseInt($scope.selectedItem.noofitems, 10);*/
 
 $scope.shipping = {};
 
@@ -49,7 +49,8 @@ $scope.billing = angular.copy($scope.shipping);
 $scope.confirmation = 0;
 $scope.confirmation += Math.floor(Math.random() * 900000);
 
-
+$('body').removeClass('modal-open');
+$('.modal-backdrop').remove();
 
 $scope.getProductCount=function(start,end){
    var temp=[];
@@ -59,8 +60,10 @@ $scope.getProductCount=function(start,end){
  return temp;
 }
 
-/*$scope.selectedMax = parseInt($scope.selectedMax);*/
+$scope.quantity = ($scope.selectedMax * $scope.selectedItem.noofitems);
 
+/*$scope.selectedMax = parseInt($scope.selectedMax, 10);
+*/
 
 
 
